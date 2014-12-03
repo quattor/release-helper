80e4393dc1b68f441ad74da47b3fc17b8b4791be
Quattor Releaser <quattor-releaser@users.noreply.github.com>
2014-12-03 14:53:29 +0000

    Render burndown as part of normal operation

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Collecting..." && ./github_pulls_collect.py && echo "Rendering..." && ./github_pulls_render.py && echo "Serving..." && python -m SimpleHTTPServer
+echo "Collecting..." && ./github_pulls_collect.py && echo "Rendering..." && ./github_pulls_render.py && ./github_pulls_render_burndown.py && echo "Serving..." && python -m SimpleHTTPServer
