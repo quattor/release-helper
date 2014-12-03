d77db53f5835d3d2a1ea9352d83190d17c43fb93
Quattor Releaser <quattor-releaser@users.noreply.github.com>
2014-12-03 14:54:13 +0000

    Use CDNs for external deps and other changes related to rendering

--- github_pulls_render.py
+++ github_pulls_render.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # encoding: utf8
 
-HEADER = '''
-<!DOCTYPE html>
+HEADER = '''<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Quattor Backlog</title>
-    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="octicons/octicons.css">
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/octicons/2.0.2/octicons.css">
     <style type="text/css">
       @import url(http://fonts.googleapis.com/css?family=Lato:400);
       body {
@@ -32,32 +31,38 @@ HEADER = '''
         color: #6cc644;
       }
       .text-github-closed {
-        color: #bd2c00;
+        color: #6E5494;
       }
       .thing-closed {
-        opacity: 0.5;
+        display: none;
+        opacity: 0.75;
       }
     </style>
-    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="/bootstrap/js/bootstrap.min.js"></script>
+    <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.3/moment.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.2.2/moment-timezone.min.js"></script>
   </head>
   <body>
     <div class="navbar navbar-inverse navbar-static-top" role="navigation">
       <div class="container">
-        <a class="navbar-brand"><img src="img/quattor_logo_navbar.png" width="94" height="23" alt="quattor logo"> &mdash; Release Backlog</a>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <!--
-              <li><a href="/#overview">PR Overview</a></li>
-              <li><a href="/#detail">PR Detail</a></li>
-            -->
-          </ul>
-        </div>
+        <a class="navbar-brand"><img src="/img/quattor_logo_navbar.png" width="94" height="23" alt="quattor logo"> &mdash; Releasing</a>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="/release/">Backlog</a></li>
+          <li><a href="/release/burndown.html">Burndown</a></li>
+        </ul>
       </div>
     </div>
 '''
 
 FOOTER = '''
+  <script type="text/javascript">
+    $(function() {
+      $('.reldate').each(function(index) {
+        $(this).text(moment.tz($(this).text(), "YYYY-MM-DDTHH:mm:ss", "UTC").fromNow());
+      });
+    });
+  </script>
   </body>
 </html>
 '''
@@ -68,29 +73,6 @@ OUTFILE = 'index.html'
 from json import load
 from cgi import escape
 from datetime import datetime
-
-
-def prettydate(d):
-    diff = datetime.utcnow() - d
-    s = diff.seconds
-    if diff.days > 28 or diff.days < 0:
-        return d.strftime('on %d %b %Y')
-    elif diff.days == 1:
-        return '1 day ago'
-    elif diff.days > 1:
-        return '%d days ago' % (diff.days)
-    elif s <= 1:
-        return 'just now'
-    elif s < 60:
-        return '%d seconds ago' % (s)
-    elif s < 120:
-        return '1 minute ago'
-    elif s < 3600:
-        return '%d minutes ago' % (s/60)
-    elif s < 7200:
-        return '1 hour ago'
-    else:
-        return '%d hours ago' % (s/3600)
 
 
 # Render data
@@ -109,7 +91,7 @@ with open('/tmp/github-pulls.json') as f_in:
         f.write(HEADER)
 
         f.write('<div class="container">\n')
-        f.write('''<button class="btn btn-default btn-sm pull-right" onclick="$('.thing-closed').toggle()">Show/Hide Closed Items</button>\n''')
+        f.write('''<button class="btn btn-default btn-sm pull-right" onclick="$('.thing-closed').toggle('slow')">Show/Hide Closed Items</button>\n''')
 
         f.write('<ul class="nav nav-tabs" role="tablist">\n')
         for milestone in milestones:
@@ -147,7 +129,7 @@ with open('/tmp/github-pulls.json') as f_in:
                 print "        " + repo
                 r = data[milestone][repo]
                 if r['due']:
-                    m_due = prettydate(datetime.strptime(r['due'], '%Y-%m-%dT%H:%M:%S'))
+                    m_due = r['due']
 
 
                 things = r['things']
@@ -156,11 +138,18 @@ with open('/tmp/github-pulls.json') as f_in:
                     f.write('<td class="col-md-3"><b>%s</b></td>\n' % (repo))
                     f.write('<td>')
                     f.write('<ul class="list-unstyled">\n')
-                    things = sorted(things, key=lambda k: k['updated'], reverse=True)
+                    things = sorted(things, key=lambda k: k['title'], reverse=True)
                     for t in things:
                         if not (milestone == 'Unassigned' and t['state'] != 'open'):
-                            t['created'] = prettydate(datetime.strptime(t['created'], '%Y-%m-%dT%H:%M:%S')) # Overwrite datetime with fuzzier version
-                            t['updated'] = prettydate(datetime.strptime(t['updated'], '%Y-%m-%dT%H:%M:%S')) # Overwrite datetime with fuzzier version
+                            t['icon'] = 'issue-opened'
+
+                            if t['type'] == 'issue' and t['state'] == 'closed':
+                                t['icon'] = 'issue-closed'
+                            elif t['type'] == 'pull-request' and t['state'] == 'open':
+                                t['icon'] = 'git-pull-request'
+                            elif t['type'] == 'pull-request' and t['state'] == 'closed':
+                                t['icon'] = 'git-merge'
+
                             f.write('<li class="thing-%(state)s">' % t)
                             f.write('<span class="text-github-%(state)s octicon octicon-%(icon)s" title="State: %(state)s"></span> ' % t)
                             f.write('<a href="%(url)s"><strong>%(title)s</strong></a> ' % t)
@@ -168,9 +157,11 @@ with open('/tmp/github-pulls.json') as f_in:
                                 f.write('<span class="text-muted"><span class="octicon octicon-comment-discussion"></span> %(comment_count)s</span> ' % t)
 
                             f.write('<div style="margin-left: 16px;">')
-                            f.write('<span class="text-muted">Created by</span> <a href="http://www.github.com/%(user)s">%(user)s</a> <span class="text-muted">%(created)s,</span> ' % t)
-                            f.write('<span class="text-muted">Updated %(updated)s</span> ' % t)
-                            f.write('<div>')
+                            f.write('<span class="text-muted">Created by</span> <a href="http://www.github.com/%(user)s">%(user)s</a> <span class="text-muted"><span class="reldate">%(created)s</span></span>' % t)
+                            if t['assignee']:
+                                f.write('<span class="text-muted">, Assigned to</span> <a href="http://www.github.com/%(assignee)s">%(assignee)s</a>' % t)
+                            f.write('<span class="text-muted">, Updated <span class="reldate">%(updated)s</span></span> ' % t)
+                            f.write('</div>')
 
                             f.write('</li>\n' % t)
                     f.write('</ul>\n')
@@ -184,7 +175,7 @@ with open('/tmp/github-pulls.json') as f_in:
 
                 f.write(
                  '''<div class="panel-footer">
-                        Progress: <span class="text-muted">(%(closed)d closed, %(open)d open, due %(due)s)</span>
+                        Progress: <span class="text-muted">(%(closed)d closed, %(open)d open, due <span class="reldate">%(due)s</span>)</span>
                         <div class="progress" style="background-color: #ccc;">
                             <div class="progress-bar" role="progressbar" aria-valuenow="%(progress)d" aria-valuemin="0" aria-valuemax="100" style="width: %(progress)d%%;">%(progress)d%%</div>
                         </div>
@@ -194,7 +185,7 @@ with open('/tmp/github-pulls.json') as f_in:
             f.write('</div>\n')
 
         f.write('</div>\n')
-        f.write('</div>\n')
+        f.write('<p class="text-muted">Page generated <span class="reldate">%s<span></p>\n' % datetime.utcnow().replace(microsecond=0).isoformat(' '))
         f.write('</div>\n')
 
         f.write(FOOTER)
